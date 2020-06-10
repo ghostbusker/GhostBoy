fa4d95ccc4f027da4a2cf784565c97473d126773
ghostbusker <austin9lee@gmail.com>
2020-06-10 04:25:07 -0400

    flattened folder structure

--- ghostboy.sh
+++ ghostboy.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 # ghostbusker's GhostBoy BuskPod installer
 
+sudo touch /boot/ssh
+
 sudo apt update
 #sudo apt dist-upgrade
 sudo apt -y install git
 sudo apt -y install cmus
-sudo git clone https://github.com/ghostbusker/BuskPod
-cd BuskPod
+sudo git clone https://github.com/ghostbusker/GhostBoy
+cd GhostBoy
 sudo mv ghostboy.theme ~/.config/cmus/
 sudo mv xboxdrv.cfg ~/.config/
+sudo mv dpi24.dtbo /boot/overlays/
+sudo mv pwm-audio-pi-zero.dtbo /boot/overlays/
+sudo mv config.txt /boot/
 sudo mkdir ~/Music
-sudo cmus -add ~/Music/
+#sudo cmus -add ~/Music/
 
 
+# add the following lines to /etc/rc.local BEFORE the line "exit 0"
+# xboxdrv --disable-kenel-driver --config ~/.config/xboxdrv.cfg &
+# cmus --show-cursor
